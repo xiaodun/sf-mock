f3db31c9acf1ca9adc9068f2ee2245fc310bb912
Xiao dun <1947147481@qq.com>
2021-04-02 23:57:05 +0800

    支持不同的方法

--- data/example/example-mock-api.js
+++ data/example/example-mock-api.js
@@ -1,5 +1,12 @@
 (function getApis() {
   return {
+    //简单使用
+    "/api/simple/object": {
+      body: {
+        message: "普通对象",
+        color: "red",
+      },
+    },
     "/api/simple/null": {
       body: null,
     },
@@ -15,10 +22,18 @@
     "/api/simple/array": {
       body: [1, 2, 3, 4, 5, 6],
     },
-    "/api/simple/object": {
+    //区分方法
+    "/api/method": {
       body: {
-        message: "普通对象",
-        color: "red",
+        // get: [1, 2, 3, 4, 5, 6],
+        get: {
+          color: "red",
+          message1: "方法指的是request支持的方法",
+          message2: "返回数据支持全部简单模式",
+        },
+      },
+      options: {
+        ingoreMethod: false,
       },
     },
   };
