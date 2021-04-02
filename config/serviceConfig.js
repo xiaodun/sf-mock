f9b5d4d8229c4c4c847ef652c2a3ea1495086880
Xiao dun <1947147481@qq.com>
2021-04-02 23:39:13 +0800

    重命名配置文件

--- config/serviceConfig.js
+++ config/serviceConfig.js
@@ -3,7 +3,7 @@
     startPort: 9192,
     rspHeaders: {
       "Content-Type": "application/json,charset=utf-8",
-      //不用nginx代理时需要添加
+      //处理跨域问题
 
       "Access-Control-Allow-Methods": "DELETE,PUT,POST,GET,OPTIONS",
       "Access-Control-Allow-Origin": "${remoteOrigin}",
