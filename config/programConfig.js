36ddb4735b81bfcab42e612ec7ce4058800fa3d8
Xiao dun <1947147481@qq.com>
2021-07-04 21:18:45 +0800

    feat: 同步更改

--- config/programConfig.js
+++ config/programConfig.js
@@ -25,5 +25,91 @@
       ],
       webpackHotUrl,
     },
+
+    "aiwen-user-h5": {
+      programUrl: "http://localhost:8088",
+      programPrefix: "/",
+      apiPrefixList: ["/api"],
+      serverList: [
+        {
+          port: 9060,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+    "ncme-app-h5": {
+      programUrl: "http://localhost:8085/h5",
+      programPrefix: "/",
+      apiPrefixList: ["/medic-h5"],
+      serverList: [
+        {
+          port: 9050,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+    "graphic-consultation-patient-h5": {
+      programUrl: " http://localhost:8080",
+      programPrefix: "/patient_consult_h5",
+      apiPrefixList: ["/p/h5/"],
+      serverList: [
+        {
+          port: 9000,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+
+    "consultation-room-pc": {
+      programUrl: "http://127.0.0.1:3888",
+      programPrefix: "/",
+      apiPrefixList: ["/api", "/message/"],
+      serverList: [
+        {
+          port: 9010,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+    aiwen_doctor_app_h5: {
+      programUrl: "http://localhost:8080",
+      programPrefix: "/",
+      apiPrefixList: ["/api"],
+      serverList: [
+        {
+          port: 9020,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+    "c-platform-h5-nuxt": {
+      programUrl: "http://localhost:56005",
+      programPrefix: "/",
+      apiPrefixList: ["/iw/p/h5", "/api"],
+      serverList: [
+        {
+          port: 9030,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
+    saas_server_admin: {
+      programUrl: "http://localhost:8896",
+      programPrefix: "/",
+      apiPrefixList: ["/api", "/medic-admin", "/v1"],
+      serverList: [
+        {
+          port: 9040,
+          isMock: true,
+        },
+      ],
+      webpackHotUrl,
+    },
   };
 })();
