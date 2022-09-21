3e3fe78cfb9f5d7c5a1169da3b568468a772acfb
王旭 <1947147481@qq.com>
2022-09-21 11:12:23 +0800

    feat: 为每个环境配置域名，便于区分

--- generate/writeNginxConfig.js
+++ generate/writeNginxConfig.js
@@ -3,11 +3,19 @@
  * 只会覆盖#program-service-start 到  #program-service-end 之间的内容
  */
 const fs = require("fs");
+const hostsFilePath = "C:\\Windows\\System32\\drivers\\etc\\hosts";
 const nginxConfContent = fs.readFileSync(
   "../nginx-1.19.6/conf/nginx.conf",
   "utf-8"
 );
-const nginxConfRegexp = /#program-service-start([\s\S]*?)#program-service-end/g;
+const mockServicePrefixFlag = "#program-service-start";
+const mockServiceSuffixFlag = "#program-service-end";
+/#program-service-start([\s\S]*?)#program-service-end/g;
+
+const mockServiceAreaRegexp = new RegExp(
+  `${mockServicePrefixFlag}([\\s\\S]*?)${mockServiceSuffixFlag}`,
+  "g"
+);
 const programConfigJs = eval(
   fs.readFileSync("../config/programConfig.js", "utf-8")
 );
@@ -18,6 +26,7 @@ const commonUtils = require("../utils/commonUtils");
 const ip = commonUtils.getIp();
 const mockServiceUrl = "http://" + ip + ":" + serviceConfig.startPort;
 const serverConfigList = [];
+const server_nameList = [];
 for (const programName in programConfigJs) {
   const programConfigs = programConfigJs[programName];
   const programValues = programConfigJs[programName];
@@ -26,6 +35,8 @@ for (const programName in programConfigJs) {
   }
   programConfigs.serverList.forEach((serverConfig) => {
     let url = serverConfig.isMock ? mockServiceUrl : serverConfig.url;
+    const server_name = commonUtils.getProxyDomain(serverConfig, programName);
+    server_nameList.push(server_name);
     serverConfigList.push(
       generateServerConfig({
         programPrefix: programConfigs.programPrefix,
@@ -36,15 +47,42 @@ for (const programName in programConfigJs) {
         ...serverConfig,
         autoAddPrefix: serverConfig.autoAddPrefix !== false ? true : false,
         webpackHotUrl: programConfigs.webpackHotUrl,
+        server_name,
       })
     );
   });
 }
 const generateNginxConfContent = nginxConfContent.replace(
-  nginxConfRegexp,
+  mockServiceAreaRegexp,
   (all, group1) => all.replace(group1, "\n" + serverConfigList.join("\n"))
 );
+
 fs.writeFileSync("../nginx-1.19.6/conf/nginx.conf", generateNginxConfContent);
+
+let generateHostsFileContent;
+const hostsFileContent = fs.readFileSync(hostsFilePath, "utf-8");
+let server_nameWriteHostsContent = server_nameList
+  .map((item) => `${ip} ${item}`)
+  .join("\n");
+if (mockServiceAreaRegexp.test(hostsFileContent)) {
+  //替换
+  generateHostsFileContent = hostsFileContent.replace(
+    mockServiceAreaRegexp,
+    (all, group1) =>
+      all.replace(group1, "\n" + server_nameWriteHostsContent + "\n")
+  );
+} else {
+  //添加
+  generateHostsFileContent = [
+    hostsFileContent,
+    mockServicePrefixFlag,
+    server_nameWriteHostsContent,
+    mockServiceSuffixFlag,
+  ].join("\n");
+}
+
+fs.writeFileSync(hostsFilePath, generateHostsFileContent);
+
 /**
  * 生成nginx.conf 的 server配置
  */
@@ -52,6 +90,7 @@ function generateServerConfig(config) {
   /**
    * /sockjs-node  用于webpack的热更新
    */
+
   const jointConfigs = config.apiPrefixList
     .map((prefix) => {
       return `location ${prefix} {
@@ -65,6 +104,7 @@ function generateServerConfig(config) {
   const serverConfig = `
     server {
         listen ${config.port};
+        server_name ${config.server_name};
         #WebServer
         ${jointConfigs}
         location ${config.programPrefix} {
--- utils/commonUtils.js
+++ utils/commonUtils.js
@@ -1,28 +1,56 @@
-const os = require('os');
+const os = require("os");
+
+//解析真实链接获取到环境信息
+
+function getEnvKeyByUrl(url) {
+  if (/rebuild-dev/g.test(url)) {
+    return "rebuild-dev";
+  } else if (/rebuild-test/g.test(url)) {
+    return "rebuild-test";
+  } else if (/dev/g.test(url)) {
+    return "dev";
+  } else if (/test/g.test(url)) {
+    return "test";
+  } else if (/uat/g.test(url)) {
+    return "uat";
+  }
+  return "prd";
+}
 
 module.exports = {
-    //替换插值
-    replaceInterpolation(str = "", data = {}) {
-
-        let result = str;
-        for (const key in data) {
-            const regexp = new RegExp(`\\$\\{${key}\\}`, 'g');
-            result = result.replace(regexp, data[key])
-        }
-        return result;
-    },
-    //获取本机的IP地址
-    getIp() {
-        let ip = "";
-        let network = os.networkInterfaces();
-        for (let key in network) {
-            let env = network[key];
-            for (var i = 0; i < env.length; i++) {
-                if (env[i].family == 'IPv4' && env[i].address != '127.0.0.1') {
-                    ip = env[i].address;
-                }
-            }
-        }
-        return ip;
+  getProxyDomain(serverConfig, programName) {
+    let envKey = serverConfig.envKey;
+    if (!envKey) {
+      if (serverConfig.isMock) {
+        envKey = "mock";
+      } else {
+        envKey = getEnvKeyByUrl(serverConfig.url);
+      }
     }
-}
+
+    return `wx.${envKey}.${programName}`;
+  },
+  //替换插值
+  replaceInterpolation(str = "", data = {}) {
+    let result = str;
+    for (const key in data) {
+      const regexp = new RegExp(`\\$\\{${key}\\}`, "g");
+      result = result.replace(regexp, data[key]);
+    }
+    return result;
+  },
+  //获取本机的IP地址
+  getIp() {
+    let ip = "";
+    let network = os.networkInterfaces();
+    for (let key in network) {
+      let env = network[key];
+      for (var i = 0; i < env.length; i++) {
+        if (env[i].family == "IPv4" && env[i].address != "127.0.0.1") {
+          ip = env[i].address;
+        }
+      }
+    }
+    return ip;
+  },
+};
