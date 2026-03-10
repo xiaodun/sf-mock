718a2d0659e4742f06bd6197f068eef9a803f474
xu.wang <xu.wang@hytechc.com>
2026-03-10 18:10:32 +0800

    被代理的前端应用可以获取访问的ip和端口
    服务端渲染时用时候需要用到转发前的ip和端口

--- generate/writeNginxConfig.js
+++ generate/writeNginxConfig.js
@@ -109,6 +109,10 @@ function generateServerConfig(config) {
         ${jointConfigs}
         location ${config.programPrefix} {
            proxy_pass ${config.programUrl};
+           proxy_set_header Host $http_host;
+           proxy_set_header X-Real-IP $remote_addr;
+           proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+           proxy_set_header X-Forwarded-Proto $scheme;
         }
         location /sockjs-node {
            proxy_set_header X-Real-IP $remote_addr;
